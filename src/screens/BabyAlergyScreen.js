f3c8d7346b3d418a88cc105a60b0e86ff5a2c6e0
nozzilee <nozzilee@gmail.com>
2020-07-24 10:22:12 +0900

    우리아이 등록하기 수정

--- src/screens/BabyAlergyScreen.js
+++ src/screens/BabyAlergyScreen.js
@@ -72,7 +72,6 @@ export default class BabyAlergy extends PureComponent {
   }
 
   render() {
-
     let screenMargin = ScreenMargin.getMargin(this.props.route.name);
 
     let CATEGORY_ROW = [];
@@ -81,10 +80,10 @@ export default class BabyAlergy extends PureComponent {
     //   if(!CATEGORY_ROW[index]) CATEGORY_ROW[index] = [];
     //   CATEGORY_ROW[index][i%4] = item;
     // });
-    for(let i=0;i <CATEGORY.length/4;i++) {
-      if(!CATEGORY_ROW[i]) CATEGORY_ROW[i] = [];
-      for(let k=0;k<4;k++) {
-        CATEGORY_ROW[i][k] = CATEGORY[i*4+k] || null;
+    for (let i = 0; i < CATEGORY.length / 4; i++) {
+      if (!CATEGORY_ROW[i]) CATEGORY_ROW[i] = [];
+      for (let k = 0; k < 4; k++) {
+        CATEGORY_ROW[i][k] = CATEGORY[i * 4 + k] || null;
       }
     }
 
@@ -95,7 +94,7 @@ export default class BabyAlergy extends PureComponent {
         <StatusBar barStyle="dark-content" />
         <SafeAreaView />
         <ScrollView style={{backgroundColor: 'white', flex: 1}}>
-          <View style={{paddingHorizontal:screenMargin}}>
+          <View style={{paddingHorizontal: screenMargin}}>
             {/* 문구 */}
             <View
               style={{
@@ -107,7 +106,7 @@ export default class BabyAlergy extends PureComponent {
                 // width="45"
                 // height="45"
               />
-              <View style={{position: 'relative',flex:1}}>
+              <View style={{position: 'relative', flex: 1}}>
                 <SvgXml
                   xml={SVG('SPEACH')}
                   // width={Dimensions.get('screen').width}
@@ -119,16 +118,17 @@ export default class BabyAlergy extends PureComponent {
                     // backgroundColor: '#3450aaaa',
                     zIndex: 10,
                     position: 'absolute',
-                    width:'100%',
-                    
-                    height:'100%',
-                    justifyContent:'center',
+                    width: '100%',
+
+                    height: '100%',
+                    justifyContent: 'center',
                     //width: Dimensions.get('screen').width - (24 + 24 + 52 + 50),
-                    paddingRight:26,
-                    paddingLeft:43
-                    
+                    paddingRight: 26,
+                    paddingLeft: 43,
                   }}>
-                  <Text numberOfLines={2} style={{fontSize:14,fontWeight:'bold'}}>
+                  <Text
+                    numberOfLines={2}
+                    style={{fontSize: 14, fontWeight: 'bold'}}>
                     우리 아이에게 있는{'\r'}알레르기와 피부트러블을 체크해주세요
                   </Text>
                 </View>
@@ -139,7 +139,9 @@ export default class BabyAlergy extends PureComponent {
 
             {/* 식품선택 */}
             <View style={{marginTop: 26}}>
-              <Text style={{fontSize: 12, fontWeight:'bold'}}>식품알레르기</Text>
+              <Text style={{fontSize: 12, fontWeight: 'bold'}}>
+                식품알레르기
+              </Text>
               <View
                 style={{
                   justifyContent: 'center',
@@ -152,36 +154,35 @@ export default class BabyAlergy extends PureComponent {
                   style={{
                     flexDirection: 'row',
                     flexWrap: 'wrap',
-                    
+
                     // backgroundColor: 'blue',
                     // justifyContent: 'center',
                     // width: CATEWIDTH(),
-                    width:'100%'
-                }}>
-                  { 
-              CATEGORY_ROW.map((rowItem, row) => {
-                return (
-                  <View style={{justifyContent:'space-between',flexDirection:'row',width:'100%'}}>
-                    {
-                      rowItem.map((item,i) => {
-                        return(
-
-                            (item) ? (
+                    width: '100%',
+                  }}>
+                  {CATEGORY_ROW.map((rowItem, row) => {
+                    return (
+                      <View
+                        style={{
+                          justifyContent: 'space-between',
+                          flexDirection: 'row',
+                          width: '100%',
+                        }}>
+                        {rowItem.map((item, i) => {
+                          return item ? (
                             <TouchableOpacity
                               style={{
-
                                 backgroundColor: 'white',
                                 alignItems: 'center',
                                 justifyContent: 'center',
-                                marginBottom:30
-                                
+                                marginBottom: 30,
                               }}
                               onPress={() => {
                                 this.setState({
                                   [item.svg]: !this.state[item.svg],
                                 });
                               }}
-                              key={row*4+i}>
+                              key={row * 4 + i}>
                               <SvgXml
                                 xml={SVG(
                                   this.state[item.svg]
@@ -196,25 +197,22 @@ export default class BabyAlergy extends PureComponent {
                                   fontSize: 14,
                                   color: '#5d5d5d',
                                   marginTop: 5,
-                                  position:'absolute',
-                                  fontSize:14,
-                                  top:36,
-                                  textAlign:'center',
-                                  width:400,
-                                }}
-                              >
+                                  position: 'absolute',
+                                  fontSize: 14,
+                                  top: 36,
+                                  textAlign: 'center',
+                                  width: 400,
+                                }}>
                                 {item.name}
                               </Text>
-                            </TouchableOpacity>) : (<View style={{width:36,height:36}}></View>)
-                          
-                        )
-                      })
-                    }
-                  </View>
-                );
-              })}
-
-
+                            </TouchableOpacity>
+                          ) : (
+                            <View style={{width: 36, height: 36}}></View>
+                          );
+                        })}
+                      </View>
+                    );
+                  })}
                 </View>
               </View>
             </View>
@@ -224,21 +222,29 @@ export default class BabyAlergy extends PureComponent {
                 style={{
                   fontSize: 12,
                   marginBottom: 10,
-                  fontWeight:'bold'
+                  fontWeight: 'bold',
                 }}>
                 피부트러블
               </Text>
               {/*  */}
-              <View style={{flexDirection: 'row',paddingHorizontal:7, marginBottom:50}}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  paddingHorizontal: 7,
+                  marginBottom: 50,
+                }}>
                 <TouchableOpacity
                   onPress={() => {
                     this.setState({
                       skin: !this.state.skin,
                     });
                   }}
-                  style={[styles.option_button,{
-                    backgroundColor: this.state.skin ? 'white' : '#32cc73',
-                  }]}>
+                  style={[
+                    styles.option_button,
+                    {
+                      backgroundColor: this.state.skin ? 'white' : '#32cc73',
+                    },
+                  ]}>
                   <Text
                     style={{
                       color: this.state.skin ? '#32cc73' : 'white',
@@ -253,9 +259,12 @@ export default class BabyAlergy extends PureComponent {
                       skin: !this.state.skin,
                     });
                   }}
-                  style={[styles.option_button,{
-                    backgroundColor: this.state.skin ? '#32cc73' : 'white',
-                  }]}>
+                  style={[
+                    styles.option_button,
+                    {
+                      backgroundColor: this.state.skin ? '#32cc73' : 'white',
+                    },
+                  ]}>
                   <Text
                     style={{
                       color: this.state.skin ? 'white' : '#32cc73',
@@ -282,7 +291,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     margin: 6,
-  }
+  },
 });
 /*
 { 
@@ -335,4 +344,3 @@ const styles = StyleSheet.create({
     );
   })}
   */
-
